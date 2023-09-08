584e30cc48bdeb6ff9943936f0dec8fa3b4da22e
Zsiga Norbert <zsiganp@gmail.com>
2023-09-08 09:14:31 +0200

    feladat00

---
+++ 00_Valtozok/Solution_Valtozok/feladat_00/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = "Katarzyna Skowranska-Dolota";
+string nation = "Lengyel";
+int birthYear = 1983;
+string birthPlace = "Warszawa";
+double height = 1.89;
+int weight = 75;
+int hitHeight = 317;
+int blockHeight = 302;
+int number = 2;
+bool teamCaptain = true;
+char captainSign = 'c';
+
+
+Console.ReadKey();
