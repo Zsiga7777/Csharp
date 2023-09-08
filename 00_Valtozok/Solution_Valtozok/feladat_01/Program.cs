2651918de00d9e9ee6c4007c1c4e6b30ff1df38d
Zsiga Norbert <zsiganp@gmail.com>
2023-09-08 09:28:08 +0200

    feladat_01

---
+++ 00_Valtozok/Solution_Valtozok/feladat_01/Program.cs
@@ -0,0 +1,4 @@
+﻿string carModel = "Torino Cobra";
+int manifacturerYear = 1970;
+Console.Write($"A Ford autógyár {manifacturerYear}-ben gyártotta le a {carModel} SportsRoof modellt.");
+Console.ReadKey();
