8946925afb97072a8749bae352f0d34dd6f23126
Zsiga Norbert <zsiganp@gmail.com>
2023-09-09 11:24:33 +0200

    feladat_02

---
+++ 00_Valtozok/Solution_Valtozok/feladat_02/Program.cs
@@ -0,0 +1,4 @@
+﻿string series = "i7";
+int generation = 8700;
+double frequency = 4.6;
+Console.Write($"Az INTEL {series} sorozatjelzésű mikroprocesszor {generation} típusának munkafrekvenciája {frequency}GHz.");
