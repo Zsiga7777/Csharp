0380dabc783ded7dae335af2dac4602d3ecbbca1
Zsiga Norbert <zsiganp@gmail.com>
2023-09-09 11:30:17 +0200

    feladat_03

---
+++ 00_Valtozok/Solution_Valtozok/feladat_03/Program.cs
@@ -0,0 +1,6 @@
+﻿string dynasty = "Ming";
+string building = "Kínai Nagyfal";
+int lenght = 7200;
+int starterYear = 1368;
+int enderYear = 1644;
+Console.Write($"A {dynasty} dinasztia {starterYear} és {enderYear} közt építette a ma látható {building} {lenght}km hosszú szakaszát.");
