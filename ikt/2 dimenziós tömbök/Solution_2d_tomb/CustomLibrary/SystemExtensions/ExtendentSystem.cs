589a47d77c3d5983187f4e7a9d68bca74ee352f5
Zsiga Norbert <b1zsinor@vasvari.org>
2024-01-23 09:33:56 +0100

    ikt 01.23

---
+++ ikt/2 dimenziós tömbök/Solution_2d_tomb/CustomLibrary/SystemExtensions/ExtendentSystem.cs
@@ -0,0 +1,11 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace System;
+
+public static class ExtendentSystem
+{
+    public static void WrtiteToConsole(this object objectToDisplay) => Console.Write(objectToDisplay);
+ 
+
+   
+}
---
+++ ikt/2 dimenziós tömbök/Solution_2d_tomb/Feladat_01/Program.cs
@@ -0,0 +1,120 @@
+﻿using Custom.Library.ConsoleExtensions;
+
+int squareSize = ExtendentConsole.ReadInteger(1,"Kérem a 2 dimenziós tömb nagyságát: ");
+
+int[,] matrix = GetMatrix();
+
+
+// Első feladat
+int sumOfMainDiagonal = GetMainDiagonalSum(matrix);
+Console.WriteLine($"A főátló számainak összege: {sumOfMainDiagonal}");
+
+// Második feladat
+int[] secondaryDiagonalElements = GetSecondaryDiagonalElements(matrix);
+Console.WriteLine($"\nA mellékátló elemei:");
+WriteArray(secondaryDiagonalElements);
+
+// Harmadik feladat
+Console.WriteLine("\n\nA főátló feletti elemek: ");
+WriteitemsAboveMainDiagonal(matrix);
+
+// Negyedik feladat
+Console.WriteLine("\nA főátló alatti elemek: ");
+WriteitemsBelowMainDiagonal(matrix);
+
+// Ötödik feladat
+int largestNumberBelowSecondaryDiagonal = matrix[squareSize-1, squareSize-1];
+Console.WriteLine($"\nA legnagyob szám a mellékátló alatt: {largestNumberBelowSecondaryDiagonal}");
+
+// Hatodik feladat
+int smallestNumberAboveSecondaryDiagonal = matrix[0, 0];
+Console.WriteLine($"\nA legkisebb szám a mellékátló felett: {smallestNumberAboveSecondaryDiagonal}");
+
+
+int[,] GetMatrix()
+{
+    int[,] matrix = new int[squareSize, squareSize];
+    int szamlalo = 0;
+
+    for (int i = 0; i < squareSize; i++)
+    {
+        for (int j = 0; j < squareSize; j++)
+        {
+            szamlalo++;
+            matrix[i, j] = szamlalo;
+        }
+    }
+
+    return matrix;
+}
+
+int GetMainDiagonalSum(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < squareSize; i++)
+    { 
+        for (int j = 0;j < squareSize; j++)
+        {
+            if (i == j)
+            { 
+                result += matrix[i, j];
+            }
+        }
+    }
+
+    return result;
+};
+
+int[] GetSecondaryDiagonalElements(int[,] matrix)
+{
+    int[] result = new int[squareSize];
+    for (int i = 0; i < squareSize; i++)
+    { 
+        for(int j = 0; j < squareSize; j++)
+        {
+            if (j == (squareSize - i-1))
+            {
+                result[i] = matrix[i, j];
+            }
+        }
+    }
+
+    return result;
+}
+
+void WriteitemsAboveMainDiagonal(int[,] matrix)
+{ 
+    for (int i = 0;i < squareSize; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            Console.Write("      ");
+        }
+        for (int k = i + 1; k < squareSize; k++)
+        {
+            Console.Write($"[{i}, {k}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void WriteitemsBelowMainDiagonal(int[,] matrix)
+{
+    for (int i = 0; i < squareSize; i++)
+    {
+        
+        for (int j = 0; j < i; j++)
+        {
+            Console.Write($"[{i}, {j}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void WriteArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item}, ");
+    }
+}
