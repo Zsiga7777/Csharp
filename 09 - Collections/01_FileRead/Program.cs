242dd89b639faadf1ac4cd2b5c0a1435c19d4e46
Zsiga Norbert <zsiganp@gmail.com>
2024-02-15 14:57:22 +0100

    házi 02.15

--- 09 - Collections/01_FileRead/Program.cs
+++ 09 - Collections/01_FileRead/Program.cs
@@ -34,3 +34,41 @@ Console.WriteLine($"{(goodStudent ? "Van" : "Nincs")} kitűnő tanuló");
 //    - jeles, ha: 4.00 - 4.99
 //    - kitünő, ha: 5.00
 
+Dictionary<string,int> sumOfRatings = GetSumOfRatings(students) ;
+sumOfRatings.WriteCollectionToConsole();
+Dictionary<string, int> GetSumOfRatings(List<Student> students)
+{
+    Dictionary<string, int> result = new Dictionary<string, int> {
+        {"elégtelen", 0},
+        {"elégséges", 0},
+        {"jó", 0},
+        {"jeles", 0},
+        {"kitűnő", 0}
+    };
+    
+
+    foreach (Student item in students)
+    {
+        switch (item.Average)
+        {
+            case < 2:
+                result["elégtelen"] ++;
+                break;
+            case < 3:
+                result["elégséges"]++;
+                break;
+            case < 4:
+                result["jó"]++;
+                break;
+            case < 5:
+                result["jeles"]++;
+                break;
+            case >= 5:
+                result["kitűnő"]++;
+                break;
+
+        }
+    }
+
+    return result;
+};
