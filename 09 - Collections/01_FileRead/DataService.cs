52e15863df01a841d9e227b6973b17cbde5ea7bc
Zsiga Norbert <zsiganp@gmail.com>
2024-02-17 10:22:25 +0100

    02.17 házi javított

---
+++ 09 - Collections/01_FileRead/DataService.cs
@@ -0,0 +1,40 @@
+﻿    public static class DataService
+    {
+    public static Dictionary<string, int> GetSumOfRatings(List<Student> students)
+    {
+        Dictionary<string, int> result = new Dictionary<string, int> {
+        {"elégtelen", 0},
+        {"elégséges", 0},
+        {"jó", 0},
+        {"jeles", 0},
+        {"kitűnő", 0}
+    };
+
+
+        foreach (Student item in students)
+        {
+            switch (item.Average)
+            {
+                case < 2:
+                    result["elégtelen"]++;
+                    break;
+                case < 3:
+                    result["elégséges"]++;
+                    break;
+                case < 4:
+                    result["jó"]++;
+                    break;
+                case < 5:
+                    result["jeles"]++;
+                    break;
+                case >= 5:
+                    result["kitűnő"]++;
+                    break;
+
+            }
+        }
+
+        return result;
+    };
+}
+
