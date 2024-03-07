b468a64f915f0c61ab35201f90e9883c9fa61886
Zsiga Norbert <b1zsinor@vasvari.org>
2024-03-07 09:32:57 +0100

    Órai 03.07 2

---
+++ 09 - Collections/07_FileRead/Fileservice.cs
@@ -0,0 +1,35 @@
+﻿
+public static class Fileservice
+    {
+    public static async Task<List<Player>> GetStudentsAsync(string fileName)
+    {
+        List<Player> players = new List<Player>();
+        Player player = null;
+        string line = null;
+        string[] data = null;
+
+        string path = Path.Combine("source", fileName);
+
+        using FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None, 128);
+        using StreamReader sr = new StreamReader(fs, Encoding.UTF7);
+
+        await sr.ReadLineAsync();
+
+        while (!sr.EndOfStream)
+        {
+            player = new Player();
+
+            line = await sr.ReadLineAsync();
+            data = line.Split(";");
+            player.Name = data[0];
+            player.FirstMatch =DateTime.Parse( data[1]);
+            player.LastMatch = DateTime.Parse(data[2]);
+            player.Weight = int.Parse(data[3]);
+            player.Height = int.Parse(data[4]);
+            players.Add(player);
+        }
+
+        return players;
+    }
+}
+
---
+++ 09 - Collections/07_FileRead/Modell/Player.cs
@@ -0,0 +1,23 @@
+﻿public class Player
+    {
+    public string Name { get; set; }
+    public DateTime FirstMatch { get; set; }
+    public DateTime LastMatch { get; set; }
+
+    public int Weight { get; set; }
+
+    public int Height { get; set; }
+
+    public Player() { }
+
+    public Player(string name, DateTime firstMatch, DateTime lastMatch, int weight, int height)
+    {
+        Name = name;
+        FirstMatch = firstMatch;
+        LastMatch = lastMatch;
+        Weight = weight;
+        Height = height;
+    }
+}
+
+
